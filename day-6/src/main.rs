4675beeef81cea7a8c7f9ee2ab263aebc39d0b3e
Niphram <theniphram@gmail.com>
2022-12-06 08:15:13 +0100

    Cleanup and comments

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,27 +1,20 @@
-use std::collections::HashSet;
+/// Find the first substring of `length` characters and returns it's index
+fn find_marker(input: &str, length: usize) -> Option<usize> {
+    // This will only work with an ascii input
+    if !input.is_ascii() {
+        return None;
+    }
 
-/// Compute the solution to part 1
-fn part_1(input: &str) -> String {
     let idx = input
+        // Treat input as a byte slice
         .as_bytes()
-        .windows(4)
-        .position(|w| w.into_iter().collect::<HashSet<_>>().len() == 4)
-        .unwrap()
-        + 4;
+        // Look at `length` bytes at a time
+        .windows(length)
+        // Find the first window, where every byte is unique
+        .position(|w| w.iter().collect::<std::collections::HashSet<_>>().len() == length);
 
-    idx.to_string()
-}
-
-/// Compute the solution to part 2
-fn part_2(input: &str) -> String {
-    let idx = input
-        .as_bytes()
-        .windows(14)
-        .position(|w| w.into_iter().collect::<HashSet<_>>().len() == 14)
-        .unwrap()
-        + 14;
-
-    idx.to_string()
+    // Add `length` to correct the index
+    idx.and_then(|idx| Some(idx + length))
 }
 
 fn main() {
@@ -29,8 +22,8 @@ fn main() {
 
     println!("Advent of Code 2022 - Day 6");
 
-    let part_1 = part_1(input);
-    let part_2 = part_2(input);
+    let part_1 = find_marker(&input, 4).expect("Index of marker");
+    let part_2 = find_marker(&input, 14).expect("Index of marker");
 
     println!("Part 1: {part_1}");
     println!("Part 2: {part_2}");
