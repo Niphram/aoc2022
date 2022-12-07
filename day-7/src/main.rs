e047ece29c54477822f70f802c465bae75d451dc
Niphram <theniphram@gmail.com>
2022-12-07 15:27:28 +0100

    Need to rewrite this later to get better

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -13,16 +13,24 @@ struct Directory {
 }
 
 impl Directory {
-    fn new(parent: Option<Weak<RefCell<Directory>>>) -> Self {
+    fn new() -> Self {
         Self {
-            parent,
+            parent: None,
             subdirs: HashMap::new(),
             files: HashMap::new(),
         }
     }
 
-    fn add_dir(&mut self, name: String, dir: Rc<RefCell<Directory>>) {
-        self.subdirs.insert(name, dir);
+    fn with_parent(parent: Weak<RefCell<Directory>>) -> Self {
+        Self {
+            parent: Some(parent),
+            subdirs: HashMap::new(),
+            files: HashMap::new(),
+        }
+    }
+
+    fn add_dir(&mut self, name: String, dir: Directory) {
+        self.subdirs.insert(name, Rc::new(RefCell::new(dir)));
     }
 
     fn add_file(&mut self, name: String, size: usize) {
@@ -41,38 +49,19 @@ impl Directory {
         files_size + subdirs_size
     }
 
-    fn size2(&self) -> (usize, usize) {
-        let subdirs_size: Vec<_> = self
+    fn get_sizes(&self) -> Vec<usize> {
+        let own_size = self.size();
+
+        let subdir_iter = self
             .subdirs
             .iter()
-            .map(|(_, dir)| RefCell::borrow(dir).borrow().size2())
-            .collect();
+            .flat_map(|(_, dir)| RefCell::borrow(dir).borrow().get_sizes());
 
-        let mut total: usize = subdirs_size.iter().map(|s| s.0).sum();
-        let mut real: usize = subdirs_size.iter().map(|s| s.1).sum();
+        let mut subdir_sizes: Vec<usize> = subdir_iter.collect();
 
-        let files_size: usize = self.files.iter().map(|(_, size)| size).sum();
+        subdir_sizes.push(own_size);
 
-        if files_size + real <= 100000 {
-            total += files_size + real;
-        }
-
-        (total, real + files_size)
-    }
-
-    fn closest_to(&self, min_size: usize) -> usize {
-        let subdirs = self
-            .subdirs
-            .iter()
-            .map(|(_, dir)| RefCell::borrow(dir).borrow().closest_to(min_size))
-            .filter(|s| *s >= min_size)
-            .min();
-
-        if let Some(size) = subdirs {
-            size
-        } else {
-            self.size()
-        }
+        subdir_sizes
     }
 
     fn get_subdir(&self, name: String) -> Rc<RefCell<Directory>> {
@@ -115,9 +104,8 @@ fn parse_ls(input: &str) -> Entry {
     }
 }
 
-/// Compute the solution to part 1
-fn part_1(input: &str) -> String {
-    let root_dir = Rc::new(RefCell::new(Directory::new(None)));
+fn parse_dir(input: &str) -> Directory {
+    let root_dir = Rc::new(RefCell::new(Directory::new()));
 
     let mut cur_dir = Rc::clone(&root_dir);
 
@@ -133,13 +121,11 @@ fn part_1(input: &str) -> String {
 
                     match entry {
                         Entry::File(name, size) => {
-                            RefCell::borrow_mut(&cur_dir).add_file(name.into(), size);
+                            cur_dir.borrow_mut().add_file(name.into(), size);
                         }
                         Entry::Dir(name) => {
-                            let subdir = Directory::new(Some(Rc::downgrade(&cur_dir)));
-
-                            RefCell::borrow_mut(&cur_dir)
-                                .add_dir(name.into(), Rc::new(RefCell::new(subdir)));
+                            let subdir = Directory::with_parent(Rc::downgrade(&cur_dir));
+                            cur_dir.borrow_mut().add_dir(name.into(), subdir);
                         }
                     }
                 }
@@ -150,82 +136,47 @@ fn part_1(input: &str) -> String {
                 }
                 ".." => {
                     let cur_dir_clone = Rc::clone(&cur_dir);
+                    let parent = &RefCell::borrow(&cur_dir_clone).parent;
 
-                    if let Some(parent) = &RefCell::borrow(&cur_dir_clone).parent {
+                    if let Some(parent) = parent {
                         let parent = Weak::upgrade(parent).unwrap();
                         cur_dir = parent;
                     };
                 }
                 d => {
-                    let cur_dir_clone = Rc::clone(&cur_dir);
-
-                    let child = RefCell::borrow(&cur_dir_clone).get_subdir(d.into());
-
-                    cur_dir = child;
+                    let subdir = cur_dir.borrow_mut().get_subdir(d.into());
+                    cur_dir = subdir;
                 }
             },
         }
     }
 
-    let size = RefCell::borrow(&root_dir).size2();
-    size.0.to_string()
+    Rc::try_unwrap(root_dir).unwrap().into_inner()
+}
+
+/// Compute the solution to part 1
+fn part_1(input: &str) -> String {
+    let root_dir = parse_dir(input);
+
+    let size: usize = root_dir.get_sizes().iter().filter(|s| **s <= 100000).sum();
+    size.to_string()
 }
 
 /// Compute the solution to part 2
 fn part_2(input: &str) -> String {
-    let root_dir = Rc::new(RefCell::new(Directory::new(None)));
+    let root_dir = parse_dir(input);
 
-    let mut cur_dir = Rc::clone(&root_dir);
+    let total = root_dir.size();
 
-    for command_str in input.split('$').skip(1) {
-        let lines: Vec<_> = command_str.lines().collect();
+    let need_to_free = total - 400000;
 
-        let command = parse_command(lines[0]);
+    let size: usize = *root_dir
+        .get_sizes()
+        .iter()
+        .filter(|s| **s >= need_to_free)
+        .min()
+        .unwrap();
 
-        match command {
-            Command::Ls() => {
-                for entry in lines[1..].iter() {
-                    let entry = parse_ls(entry);
-
-                    match entry {
-                        Entry::File(name, size) => {
-                            RefCell::borrow_mut(&cur_dir).add_file(name.into(), size);
-                        }
-                        Entry::Dir(name) => {
-                            let subdir = Directory::new(Some(Rc::downgrade(&cur_dir)));
-
-                            RefCell::borrow_mut(&cur_dir)
-                                .add_dir(name.into(), Rc::new(RefCell::new(subdir)));
-                        }
-                    }
-                }
-            }
-            Command::Cd(path) => match path {
-                "/" => {
-                    cur_dir = Rc::clone(&root_dir);
-                }
-                ".." => {
-                    let cur_dir_clone = Rc::clone(&cur_dir);
-
-                    if let Some(parent) = &RefCell::borrow(&cur_dir_clone).parent {
-                        let parent = Weak::upgrade(parent).unwrap();
-                        cur_dir = parent;
-                    };
-                }
-                d => {
-                    let cur_dir_clone = Rc::clone(&cur_dir);
-
-                    let child = RefCell::borrow(&cur_dir_clone).get_subdir(d.into());
-
-                    cur_dir = child;
-                }
-            },
-        }
-    }
-
-    let total = RefCell::borrow(&root_dir).size();
-
-    let size = RefCell::borrow(&root_dir).closest_to(30000000 - (70000000 - total));
     size.to_string()
 }
 
