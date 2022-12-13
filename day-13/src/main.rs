55340241701901169873dc7a2811f1fd98689a81
Niphram <theniphram@gmail.com>
2022-12-13 12:25:41 +0100

    Cleanup and comments

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,124 +1,115 @@
 use std::cmp::Ordering;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-enum Nested {
+enum Packet {
     Values(Vec<usize>),
-    List(Vec<Nested>),
+    List(Vec<Packet>),
 }
 
-impl Ord for Nested {
+/// Implement ordering for our packet-struct
+/// Makes heavy use of rusts default ordering of Vec's (lexicographical comparison)
+impl Ord for Packet {
     fn cmp(&self, other: &Self) -> Ordering {
         match (self, other) {
-            (Self::Values(left), Self::Values(right)) => left
-                .iter()
-                .zip(right)
-                .find_map(|(left, right)| left.cmp(right).is_ne().then_some(left.cmp(right)))
-                .unwrap_or_else(|| left.len().cmp(&right.len())),
-            (Self::List(left), Self::List(right)) => left
-                .iter()
-                .zip(right)
-                .find_map(|(left, right)| left.cmp(right).is_ne().then_some(left.cmp(right)))
-                .unwrap_or_else(|| left.len().cmp(&right.len())),
-            (Self::List(left), Self::Values(right)) => {
-                Self::List(left.clone()).cmp(&Self::List(vec![Self::Values(right.clone())]))
-            }
-            (Self::Values(left), Self::List(right)) => {
-                Self::List(vec![Self::Values(left.clone())]).cmp(&Self::List(right.clone()))
-            }
+            // If both sides are Packet::Values, just compare the Vec<usize>
+            (Self::Values(left), Self::Values(right)) => left.cmp(right),
+            // If both sides are Packet::List, compare the underlying Vec<Packet>
+            (Self::List(left), Self::List(right)) => left.cmp(right),
+            // If the sides aren't the same, upgrade one side to a Vec<Packet> and compare Vec's
+            (Self::List(left), Self::Values(_)) => left.cmp(&vec![other.clone()]),
+            (Self::Values(_), Self::List(right)) => vec![self.clone()].cmp(right),
         }
     }
 }
 
-impl PartialOrd for Nested {
+/// Implemented so clippy stops shouting at me
+impl PartialOrd for Packet {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-fn parse_line(input: &str) -> Nested {
+fn parse_line(input: &str) -> Packet {
+    // Try parsing comma seperated integers
     let list = input
         .split(',')
         .map(&str::parse::<usize>)
         .collect::<Vec<_>>();
 
     if list.iter().all(Result::is_ok) {
-        Nested::Values(list.into_iter().map(|v| v.unwrap()).collect())
+        // If parsing was successful, return Packet::Values
+        Packet::Values(list.into_iter().map(|v| v.unwrap()).collect())
     } else {
-        let find_closing = |input: &str| {
-            input
+        // Split into smaller lists
+
+        // Function to find the next part
+        fn next_part(input: &str) -> Option<(&str, &str)> {
+            // Return none if input is empty
+            if input.is_empty() {
+                return None;
+            }
+
+            let end = input
                 .chars()
+                // Parse brackets to find depth of each character
                 .scan(0, |depth, c| {
                     match c {
-                        '[' => {
-                            *depth += 1;
-                        }
-                        ']' => {
-                            *depth -= 1;
-                        }
+                        '[' => *depth += 1,
+                        ']' => *depth -= 1,
                         _ => {}
                     };
 
-                    (*depth > 0).then_some(c)
+                    Some((*depth, c))
                 })
-                .count()
-        };
+                // Find the next '|' that is at depth 0
+                .position(|(depth, c)| depth == 0 && c == '|');
 
-        let mut output = vec![];
-        let mut rest = input;
-
-        while !rest.is_empty() {
-            match &rest[..1] {
-                "[" => {
-                    let end = find_closing(rest);
-                    output.push(parse_line(&rest[1..end]));
-                    rest = &rest[end + 1..];
-                }
-                "|" => {
-                    if let Some(end) = rest.find('|') {
-                        if end != 0 {
-                            output.push(parse_line(&rest[1..end]));
-                        }
-                        rest = &rest[end + 1..];
-                    } else {
-                        output.push(parse_line(&rest[1..]));
-                        break;
-                    }
-                }
-                _ => {
-                    if let Some(end) = rest.find(|c| c == '|' || c == ']') {
-                        output.push(parse_line(&rest[..end]));
-                        rest = &rest[end + 1..];
-                    } else {
-                        output.push(parse_line(rest));
-                        break;
-                    }
-                }
+            if let Some(end) = end {
+                // Return both parts if found
+                Some((&input[..end], &input[end + 1..]))
+            } else {
+                // Otherwise return rest of string and empty slice
+                Some((input, &input[0..0]))
             }
         }
 
-        Nested::List(output)
+        // Collect all Packets
+        let mut output = vec![];
+
+        // Repeat until no packets remain
+        let mut next = &input[1..input.len() - 1];
+        while let Some((part, rest)) = next_part(next) {
+            output.push(parse_line(part));
+
+            next = rest;
+        }
+
+        Packet::List(output)
     }
 }
 
-fn parse_input(input: &str) -> Vec<(Nested, Nested)> {
+fn parse_input(input: &str) -> Vec<Packet> {
+    // Use different symbols to seperate lists from values
     let input = input.replace(",[", "|[").replace("],", "]|");
-    let lines: Vec<_> = input.lines().collect();
 
-    lines
-        .chunks(3)
-        .map(|chunk| (parse_line(chunk[0]), parse_line(chunk[1])))
+    // Parse all packets, ignore empty lines
+    input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(parse_line)
         .collect()
 }
 
 /// Compute the solution to part 1
 fn part_1(input: &str) -> String {
-    let parsed = parse_input(input);
+    let packets = parse_input(input);
 
-    let res: usize = parsed
-        .iter()
+    let res: usize = packets
+        .chunks(2)
+        // Enumerate to get indices
         .enumerate()
-        .filter(|(_, (left, right))| left < right)
-        .map(|(i, ..)| i + 1)
+        // Filter only packets that are correctly ordered
+        .filter_map(|(idx, packets)| (packets[0] < packets[1]).then_some(idx + 1))
         .sum();
 
     res.to_string()
@@ -126,20 +117,22 @@ fn part_1(input: &str) -> String {
 
 /// Compute the solution to part 2
 fn part_2(input: &str) -> String {
-    let parsed = parse_input(input);
+    let mut parsed = parse_input(input);
 
-    let mut parsed: Vec<_> = parsed.iter().flat_map(|(l, r)| [l, r]).collect();
-
+    // Create marker packets
     let pack_a = parse_line("[[2]]");
     let pack_b = parse_line("[[6]]");
 
-    parsed.push(&pack_a);
-    parsed.push(&pack_b);
+    // Insert markers into packets
+    parsed.push(pack_a.clone());
+    parsed.push(pack_b.clone());
 
+    // Sort packets
     parsed.sort();
 
-    let idx_a = parsed.iter().position(|p| *p == &pack_a).unwrap() + 1;
-    let idx_b = parsed.iter().position(|p| *p == &pack_b).unwrap() + 1;
+    // Find markers in sorted packets
+    let idx_a = parsed.iter().position(|p| p == &pack_a).unwrap() + 1;
+    let idx_b = parsed.iter().position(|p| p == &pack_b).unwrap() + 1;
 
     (idx_a * idx_b).to_string()
 }
