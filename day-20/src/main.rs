50c6f4659f2f4be5309c893f7050914a9dc055ad
Niphram <theniphram@gmail.com>
2022-12-20 13:11:31 +0100

    Cleanup and comments

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -1,123 +1,70 @@
-use std::{cmp::Ordering, ops::AddAssign, time::Instant};
+fn decrypt_coordinates<const ITERATIONS: usize>(numbers: &[isize]) -> isize {
+    // Enumerate numbers to know their original indices
+    let mut numbers = numbers.iter().enumerate().collect::<Vec<_>>();
 
-struct Ring {
-    data: Vec<isize>,
+    // Count numbers
+    let len = numbers.len();
+
+    // Repeat for specified iterations
+    for _ in 0..ITERATIONS {
+        // Go through all numbers in order
+        for i in 0..len {
+            // Find number in vec
+            let idx = numbers
+                .iter()
+                .position(|(idx, _)| *idx == i)
+                .expect("Find number by original index");
+
+            // Remove from vec
+            let (orig_idx, val) = numbers.remove(idx);
+
+            // Find new index
+            let new_idx = (idx as isize + val)
+                .rem_euclid(len as isize - 1)
+                .unsigned_abs();
+
+            // Insert number at new position
+            numbers.insert(new_idx, (orig_idx, val));
+        }
+    }
+
+    // Find the index of the zero-value
+    let zero_idx = numbers
+        .iter()
+        .position(|(_, &v)| v == 0)
+        .expect("Find 0-value");
+
+    // Sum values of the elements that are offset by 1000, 2000 and 3000 from the 0-Element
+    [1000, 2000, 3000]
+        .iter()
+        .map(|o| (zero_idx + o) % len)
+        .map(|idx| numbers[idx].1)
+        .sum()
 }
 
 /// Compute the solution to part 1
 fn part_1(input: &str) -> String {
-    let mut numbers = input
+    // Parse numbers
+    let numbers = input
         .lines()
-        .map(&str::parse::<isize>)
-        .map(Result::unwrap)
-        .enumerate()
+        .flat_map(&str::parse::<isize>)
         .collect::<Vec<_>>();
 
-    let len = numbers.len();
-
-    for i in 0..len {
-        let mut pos = numbers.iter().position(|(idx, _)| *idx == i).unwrap();
-        let val = numbers[pos];
-
-        match val.1.cmp(&0) {
-            Ordering::Greater => {
-                for _ in 0..val.1 {
-                    let from = pos % len;
-                    let to = (from + 1) % len;
-
-                    numbers.swap(from, to);
-
-                    pos = to;
-                }
-            }
-            Ordering::Less => {
-                for _ in 0..val.1.abs() {
-                    let from = pos % len;
-                    let to = (from + len - 1) % len;
-
-                    numbers.swap(from, to);
-
-                    pos = to;
-                }
-            }
-            _ => {}
-        }
-    }
-
-    let zero_idx = numbers.iter().position(|(_, v)| *v == 0).unwrap();
-
-    let a = (zero_idx + 1000) % len;
-    let b = (zero_idx + 2000) % len;
-    let c = (zero_idx + 3000) % len;
-
-    (numbers[a].1 + numbers[b].1 + numbers[c].1).to_string()
+    // Decrypt one iteration
+    decrypt_coordinates::<1>(&numbers).to_string()
 }
 
 /// Compute the solution to part 2
 fn part_2(input: &str) -> String {
-    let mut numbers_orig = input
+    // Parse numbers and multiply by 811589153
+    let numbers = input
         .lines()
-        .map(&str::parse::<isize>)
-        .map(Result::unwrap)
+        .flat_map(&str::parse::<isize>)
         .map(|v| v * 811589153)
-        .enumerate()
         .collect::<Vec<_>>();
 
-    let len = numbers_orig.len();
-
-    let mut numbers = numbers_orig
-        .iter()
-        .map(|(idx, n)| match n.cmp(&0) {
-            Ordering::Greater => (*idx, *n % (len - 1) as isize),
-            Ordering::Less => (*idx, n.rem_euclid(len as isize - 1)),
-            Ordering::Equal => (*idx, *n),
-        })
-        .collect::<Vec<_>>();
-
-    for _ in 0..10 {
-        for i in 0..len {
-            let mut pos = numbers.iter().position(|(idx, _)| *idx == i).unwrap();
-            let val = numbers[pos];
-
-            match val.1.cmp(&0) {
-                Ordering::Greater => {
-                    for _ in 0..val.1 {
-                        let from = pos % len;
-                        let to = (from + 1) % len;
-
-                        numbers.swap(from, to);
-
-                        pos = to;
-                    }
-                }
-                Ordering::Less => {
-                    for _ in 0..val.1.abs() {
-                        let from = pos % len;
-                        let to = (from + len - 1) % len;
-
-                        numbers.swap(from, to);
-
-                        pos = to;
-                    }
-                }
-                _ => {}
-            }
-        }
-
-        println!("ITERATE!");
-    }
-
-    let zero_idx = numbers.iter().position(|(_, v)| *v == 0).unwrap();
-
-    let (idx_a, _) = numbers[(zero_idx + 1000) % len];
-    let (idx_b, _) = numbers[(zero_idx + 2000) % len];
-    let (idx_c, _) = numbers[(zero_idx + 3000) % len];
-
-    let num_a = numbers_orig.iter().find(|(i, n)| *i == idx_a).unwrap();
-    let num_b = numbers_orig.iter().find(|(i, n)| *i == idx_b).unwrap();
-    let num_c = numbers_orig.iter().find(|(i, n)| *i == idx_c).unwrap();
-
-    (num_a.1 + num_b.1 + num_c.1).to_string()
+    // Decrypt ten iterations
+    decrypt_coordinates::<10>(&numbers).to_string()
 }
 
 fn main() {
@@ -125,11 +72,9 @@ fn main() {
 
     println!("Advent of Code 2022 - Day 20");
 
-    let start = Instant::now();
     let part_1_res = part_1(input);
-    println!("Part 1: {part_1_res}. Took {:?}", start.elapsed());
+    println!("Part 1: {part_1_res}");
 
-    let start = Instant::now();
     let part_2_res = part_2(input);
-    println!("Part 2: {part_2_res}. Took {:?}", start.elapsed());
+    println!("Part 2: {part_2_res}");
 }
