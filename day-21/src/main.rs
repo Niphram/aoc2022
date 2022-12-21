e7d971ef7bfc487d2472d8e4f1da3f5aa7afc6e9
Niphram <theniphram@gmail.com>
2022-12-21 11:59:21 +0100

    Cleanup and comments

--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -1,102 +1,111 @@
-use std::{collections::HashMap, str::FromStr};
+use std::collections::HashMap;
 
 #[derive(Debug)]
-enum Numbers<'a> {
-    Val(isize),
-    Add(&'a str, &'a str),
-    Sub(&'a str, &'a str),
-    Mul(&'a str, &'a str),
-    Div(&'a str, &'a str),
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Equal,
 }
 
-impl<'a> From<&'a str> for Numbers<'a> {
+#[derive(Debug)]
+enum MonkeyYell<'a> {
+    Value(usize),
+    Equation(&'a str, &'a str, Operation),
+}
+
+impl<'a> From<&'a str> for MonkeyYell<'a> {
     fn from(s: &'a str) -> Self {
-        if let Ok(val) = s.parse::<isize>() {
-            Numbers::Val(val)
+        if let Ok(val) = s.parse::<usize>() {
+            Self::Value(val)
         } else {
             match s.chars().nth(5) {
-                Some('+') => Numbers::Add(&s[..4], &s[7..]),
-                Some('-') => Numbers::Sub(&s[..4], &s[7..]),
-                Some('*') => Numbers::Mul(&s[..4], &s[7..]),
-                Some('/') => Numbers::Div(&s[..4], &s[7..]),
-                _ => panic!(),
+                Some('+') => Self::Equation(&s[..4], &s[7..], Operation::Add),
+                Some('-') => Self::Equation(&s[..4], &s[7..], Operation::Sub),
+                Some('*') => Self::Equation(&s[..4], &s[7..], Operation::Mul),
+                Some('/') => Self::Equation(&s[..4], &s[7..], Operation::Div),
+                Some(op) => panic!("Unknown operation = '{op}'"),
+                None => panic!("Could not read operation!"),
             }
         }
     }
 }
 
-fn get_monkey_value(name: &str, monkeys: &HashMap<&str, Numbers>) -> isize {
-    let monkey = &monkeys[name];
+/// Traverses the tree of monkeys and returns the resulting value
+fn get_monkey_value(name: &str, monkeys: &HashMap<&str, MonkeyYell>) -> usize {
+    match &monkeys[name] {
+        // If monkey is shouting a number, return it
+        MonkeyYell::Value(val) => *val,
+        // Otherwise check both monkeys this monkey is listening for and compute answer
+        MonkeyYell::Equation(a, b, op) => {
+            let a = get_monkey_value(a, monkeys);
+            let b = get_monkey_value(b, monkeys);
 
-    match monkey {
-        Numbers::Val(val) => *val,
-        Numbers::Add(a, b) => get_monkey_value(a, monkeys) + get_monkey_value(b, monkeys),
-        Numbers::Sub(a, b) => get_monkey_value(a, monkeys) - get_monkey_value(b, monkeys),
-        Numbers::Mul(a, b) => get_monkey_value(a, monkeys) * get_monkey_value(b, monkeys),
-        Numbers::Div(a, b) => get_monkey_value(a, monkeys) / get_monkey_value(b, monkeys),
+            match op {
+                Operation::Add => a + b,
+                Operation::Sub => a - b,
+                Operation::Mul => a * b,
+                Operation::Div => a / b,
+                _ => panic!("Unsupported operation!"),
+            }
+        }
     }
 }
 
-fn contains_humn(name: &str, monkeys: &HashMap<&str, Numbers>) -> bool {
+/// Checks if the monkey includes the human in their answer
+fn listens_to_human(name: &str, monkeys: &HashMap<&str, MonkeyYell>) -> bool {
     if name == "humn" {
         true
     } else {
-        let monkey = &monkeys[name];
-
-        match monkey {
-            Numbers::Val(val) => false,
-            Numbers::Add(a, b) => contains_humn(a, monkeys) || contains_humn(b, monkeys),
-            Numbers::Sub(a, b) => contains_humn(a, monkeys) || contains_humn(b, monkeys),
-            Numbers::Mul(a, b) => contains_humn(a, monkeys) || contains_humn(b, monkeys),
-            Numbers::Div(a, b) => contains_humn(a, monkeys) || contains_humn(b, monkeys),
+        match &monkeys[name] {
+            // A monkey just yelling a number does not listen to the human
+            MonkeyYell::Value(_) => false,
+            // Check both 'monkeys' that this monkey is listening to
+            MonkeyYell::Equation(a, b, _op) => {
+                listens_to_human(a, monkeys) || listens_to_human(b, monkeys)
+            }
         }
     }
 }
 
-fn solve_for_humn(name: &str, target: isize, monkeys: &HashMap<&str, Numbers>) -> isize {
+/// Given a monkey that has to wait for the human and a target
+/// value, find the value the human has to yell
+fn solve_for_human(name: &str, target: usize, monkeys: &HashMap<&str, MonkeyYell>) -> usize {
+    // If we found the human, just return the target value
     if name == "humn" {
         target
     } else {
-        let monkey = &monkeys[name];
+        match &monkeys[name] {
+            MonkeyYell::Equation(a, b, op) => {
+                // Check which side of the equation contains the human
+                if listens_to_human(a, monkeys) {
+                    // Find out the target-value for the left side
+                    let target = match op {
+                        Operation::Add => target - get_monkey_value(b, monkeys),
+                        Operation::Mul => target / get_monkey_value(b, monkeys),
+                        Operation::Sub => get_monkey_value(b, monkeys) + target,
+                        Operation::Div => get_monkey_value(b, monkeys) * target,
+                        Operation::Equal => get_monkey_value(b, monkeys),
+                    };
 
-        match monkey {
-            Numbers::Add(a, b) => {
-                if contains_humn(a, monkeys) {
-                    solve_for_humn(a, target - get_monkey_value(b, monkeys), monkeys)
-                } else if contains_humn(b, monkeys) {
-                    solve_for_humn(b, target - get_monkey_value(a, monkeys), monkeys)
+                    solve_for_human(a, target, monkeys)
+                } else if listens_to_human(b, monkeys) {
+                    // Find out the target-value for the right side
+                    let target = match op {
+                        Operation::Add => target - get_monkey_value(a, monkeys),
+                        Operation::Mul => target / get_monkey_value(a, monkeys),
+                        Operation::Sub => get_monkey_value(a, monkeys) - target,
+                        Operation::Div => get_monkey_value(a, monkeys) / target,
+                        Operation::Equal => get_monkey_value(a, monkeys),
+                    };
+
+                    solve_for_human(b, target, monkeys)
                 } else {
-                    panic!()
+                    panic!("{name}'s answer is independent of the human!")
                 }
             }
-            Numbers::Sub(a, b) => {
-                if contains_humn(a, monkeys) {
-                    solve_for_humn(a, get_monkey_value(b, monkeys) + target, monkeys)
-                } else if contains_humn(b, monkeys) {
-                    solve_for_humn(b, get_monkey_value(a, monkeys) - target, monkeys)
-                } else {
-                    panic!()
-                }
-            }
-            Numbers::Mul(a, b) => {
-                if contains_humn(a, monkeys) {
-                    solve_for_humn(a, target / get_monkey_value(b, monkeys), monkeys)
-                } else if contains_humn(b, monkeys) {
-                    solve_for_humn(b, target / get_monkey_value(a, monkeys), monkeys)
-                } else {
-                    panic!()
-                }
-            }
-            Numbers::Div(a, b) => {
-                if contains_humn(a, monkeys) {
-                    solve_for_humn(a, get_monkey_value(b, monkeys) * target, monkeys)
-                } else if contains_humn(b, monkeys) {
-                    solve_for_humn(b, get_monkey_value(a, monkeys) / target, monkeys)
-                } else {
-                    panic!()
-                }
-            }
-            _ => panic!("Should not get a value answer"),
+            MonkeyYell::Value(_) => panic!("{name}'s answer is independent of the human!"),
         }
     }
 }
@@ -107,7 +116,7 @@ fn part_1(input: &str) -> String {
         .lines()
         .map(|line| {
             let (monkey, answer) = line.split_once(": ").unwrap();
-            let answer: Numbers = answer.into();
+            let answer: MonkeyYell = answer.into();
             (monkey, answer)
         })
         .collect::<HashMap<_, _>>();
@@ -121,32 +130,19 @@ fn part_2(input: &str) -> String {
         .lines()
         .map(|line| {
             let (monkey, answer) = line.split_once(": ").unwrap();
-            let answer: Numbers = answer.into();
+            let answer: MonkeyYell = answer.into();
             (monkey, answer)
         })
         .collect::<HashMap<_, _>>();
 
-    let root = monkeys.remove("root").unwrap();
-
-    let (left, right) = match root {
-        Numbers::Add(a, b) => (a, b),
-        Numbers::Sub(a, b) => (a, b),
-        Numbers::Mul(a, b) => (a, b),
-        Numbers::Div(a, b) => (a, b),
-        _ => panic!(),
-    };
-
-    let (humn_branch, target) = if contains_humn(left, &monkeys) {
-        // Human is in the left branch
-        (left, get_monkey_value(right, &monkeys))
-    } else if contains_humn(right, &monkeys) {
-        // Human is in the right branch
-        (right, get_monkey_value(left, &monkeys))
+    // Change root monkey's operation to 'equal'
+    if let MonkeyYell::Equation(a, b, _) = monkeys.remove("root").unwrap() {
+        monkeys.insert("root", MonkeyYell::Equation(a, b, Operation::Equal));
     } else {
-        panic!("Human is unused?")
-    };
+        panic!("Monkey 'root' can't yell a value!");
+    }
 
-    solve_for_humn(humn_branch, target, &monkeys).to_string()
+    solve_for_human("root", 0, &monkeys).to_string()
 }
 
 fn main() {
