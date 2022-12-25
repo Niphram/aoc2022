426a6b945f96a6eeb5c91eeb86e34f24e3982b23
Niphram <theniphram@gmail.com>
2022-12-25 20:35:27 +0100

    Cleanup and comments

--- day-25/src/main.rs
+++ day-25/src/main.rs
@@ -1,80 +1,91 @@
-use std::{fmt::Display, num::ParseIntError, str::FromStr};
+use std::{fmt::Display, iter::Sum, ops::Add, str::FromStr};
 
 #[derive(Debug, Clone, Copy)]
 struct Snafu(isize);
 
+impl Add for Snafu {
+    type Output = Self;
+
+    /// Adds two snafus
+    fn add(self, rhs: Self) -> Self::Output {
+        Snafu(self.0 + rhs.0)
+    }
+}
+
+impl Sum for Snafu {
+    /// Sums an iterator of snafus
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Snafu(0), |a, b| a + b)
+    }
+}
+
 impl FromStr for Snafu {
-    type Err = ParseIntError;
+    type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(
-            s.chars()
-                .rev()
-                .enumerate()
-                .map(|(i, c)| {
-                    let digit = match c {
-                        '2' => 2,
-                        '1' => 1,
-                        '0' => 0,
-                        '-' => -1,
-                        '=' => -2,
-                        _ => panic!(),
-                    };
+        let mut res = 0;
 
-                    digit * (5isize.pow(i as u32))
-                })
-                .sum(),
-        ))
+        // Go through all characters
+        for (i, c) in s.chars().rev().enumerate() {
+            // Match digits
+            let digit = match c {
+                '2' => 2,
+                '1' => 1,
+                '0' => 0,
+                '-' => -1,
+                '=' => -2,
+                _ => Err(())?,
+            };
+
+            let i = i.try_into().map_err(|_| ())?;
+
+            // Sum digits
+            res += digit * (5isize.pow(i));
+        }
+
+        Ok(Snafu(res))
     }
 }
 
 impl Display for Snafu {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut c = self.0;
+        if self.0 == 0 {
+            // If the number is 0 just write that
+            write!(f, "0")
+        } else {
+            let positive = self.0 >= 0;
+            let mut number = self.0.unsigned_abs();
 
-        let mut digits = vec![];
+            let mut result = vec![];
 
-        while c != 0 {
-            let mut rem = c % 5;
+            // Divide number by 5 until we reach 0
+            while number != 0 {
+                let remainder = number % 5;
+                number /= 5;
 
-            if rem > 2 {
-                c += 5;
-                rem -= 5;
+                // snafu numbers use -2 to 2 instead of 0 to 5, so we carry if needed
+                if remainder > 2 {
+                    number += 1;
+                }
+
+                // Push remainder to result
+                result.push(remainder);
             }
 
-            c /= 5;
+            // Lookup
+            let lookup = if positive {
+                ['0', '1', '2', '=', '-']
+            } else {
+                ['0', '-', '=', '2', '1']
+            };
 
-            digits.push(rem);
+            for digit in result.iter().rev() {
+                write!(f, "{}", lookup[*digit])?;
+            }
+
+            Ok(())
         }
-
-        for d in digits.iter().rev() {
-            write!(
-                f,
-                "{}",
-                match d {
-                    2 => '2',
-                    1 => '1',
-                    0 => '0',
-                    -1 => '-',
-                    -2 => '=',
-                    _ => panic!(),
-                }
-            )?;
-        }
-
-        Ok(())
     }
-}
-
-/// Compute the solution to part 1
-fn part_1(input: &str) -> String {
-    let fuel_sum: isize = input
-        .lines()
-        .flat_map(&str::parse::<Snafu>)
-        .map(|s| s.0)
-        .sum();
-
-    Snafu(fuel_sum).to_string()
 }
 
 fn main() {
@@ -82,6 +93,6 @@ fn main() {
 
     println!("Advent of Code 2022 - Day 25");
 
-    let part_1_res = part_1(input);
-    println!("Part 1: {part_1_res}");
+    let fuel_sum: Snafu = input.lines().flat_map(&str::parse::<Snafu>).sum();
+    println!("Part 1: {fuel_sum}");
 }
